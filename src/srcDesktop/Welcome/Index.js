adf256c6b0c92914271e798e511fa25cb2229197
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2019-02-24 11:39:08 -0700

    button style updateds

--- src/srcDesktop/Welcome/Index.js
+++ src/srcDesktop/Welcome/Index.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import flatirons from "./assets/flatirons.jpg";
-import Table from "./Table";
 
 export default class Homepage extends React.Component {
   render() {
     return (
       <div>
-        
         <img id="flatirons" src={flatirons} alt="Flatirons" />
       </div>
     );
